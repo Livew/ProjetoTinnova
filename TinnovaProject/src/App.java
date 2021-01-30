0b00359ddcd58fbfa2c427ca79fb32827a296199
Livew <andre.magalhaes.unip@gmail.com>
2021-01-30 00:24:12 -0300

    multiplos

--- TinnovaProject/src/App.java
+++ TinnovaProject/src/App.java
@@ -1,8 +1,11 @@
 import Exercicio1.Votos;
 import Exercicio3.Fatorial;
+import Exercicio4.Multiplo;
 
 public class App {
     public static void main(String[] args) throws Exception {
+
+        //Exercicio 1 - Votos
         Votos votos = new Votos(1000,800,150,50);
 
         double percentualValidos = votos.percentualVotosValidos();
@@ -13,11 +16,22 @@ public class App {
         System.out.println(percentualBrancos+"% Votos brancos");
         System.out.println(percentualNulos+"% Votos nulos");
 
+        //Exercicio 2 - Bublble
+        
 
+        //Exercicio 3 - Fatorial
         Fatorial fatorial = new Fatorial(5);
 
         int fatorialResult = fatorial.calculaFatorial();
 
         System.out.println(fatorialResult);
+
+
+        //Exercicio 4 - Multiplos
+        Multiplo multiplo = new Multiplo(20);
+
+        int soma = multiplo.somaDosMultiplos();
+
+        System.out.println(soma);
     }
 }
--- TinnovaProject/src/Exercicio4/Multiplo.java
+++ TinnovaProject/src/Exercicio4/Multiplo.java
@@ -1,5 +1,21 @@
 package Exercicio4;
 
 public class Multiplo {
-    
+    int numero;
+
+    public Multiplo(int numero){
+        this.numero = numero;
+    }
+
+    public int somaDosMultiplos(){
+        int somaMultiplos = 0;
+
+        for(int i = numero-1; i >= 0; i--){
+            if (i % 3 == 0 || i % 5 == 0){
+                somaMultiplos = somaMultiplos + i;
+            }
+        }
+
+        return somaMultiplos;
+    }
 }
